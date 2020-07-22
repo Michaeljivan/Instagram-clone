1283eec043b2c2d868133c289b096e58cdbbcf30
Michael Jivan <michaeljivan@Michaels-MacBook-Pro.local>
2020-07-22 09:58:06 -0700

    header added

---
+++ src/components/Post.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Post() {
+    return (
+        <div>
+            {/* header -> avatar + username */}
+            <h3>username</h3>
+            {/* image */}
+            <img src={''} alt={''}/>
+            {/* username + caption */}
+            <h4>username: Captions</h4>
+        </div>
+    )
+}
+
+export default Post;
