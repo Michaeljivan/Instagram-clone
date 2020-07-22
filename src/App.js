7856d330ab0fccfd8b2aad603a801d38122f981d
Michaeljivan <mjivan7@gmail.com>
2020-07-22 13:17:04 -0700

    Posts added, useState

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 //components
 import Post from './components/Post';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {
+      username: 'hustleteam95',
+      caption: 'I love instagram',
+      imageUrl: 'https://pluspng.com/img-png/instagram-png-instagram-logo-2-png-8-de-abril-de-2017-927-kb-3500-3393-3500.png'
+    },
+    {
+      username: 'jivanm',
+      caption: 'World War Z looks dope',
+      imageUrl: 'https://www.playstationlifestyle.net/assets/uploads/2019/04/World_War_Z-Screenshot_34_logo.png'
+    },
+    {
+      username: 'rsswam',
+      caption: 'I like flowers',
+      imageUrl: 'https://i.pinimg.com/originals/98/22/77/9822777147bff1a2de272cd8f2c31456.jpg'
+    },
+  ]);
+
   return (
     <div className={'app'}>
       <div className={'app_header'}>
@@ -13,9 +31,11 @@ function App() {
             alt={'Instagram Logo'}
         />
       </div>
-      {/* Posts */}
-      <Post />
-        {/* Posts */}
+      {
+        posts.map(post => (
+          <Post username={post.username} imageUrl={post.imageUrl} caption={post.caption} />
+        ))
+      }
     </div>
   );
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -1,14 +1,22 @@
 import React from 'react'
+import '../post.css'
+import Avatar from '@material-ui/core/Avatar';
 
-function Post() {
+function Post({imageUrl, username, caption}) {
     return (
-        <div>
-            {/* header -> avatar + username */}
-            <h3>username</h3>
-            {/* image */}
-            <img src={''} alt={''}/>
-            {/* username + caption */}
-            <h4>username: Captions</h4>
+        <div className={'post'}>
+            <div className={'post_header'}>
+                <Avatar
+                    className={'post_avatar'}
+                    alt={'username'}
+                    src={''}
+                />
+                <h3>{username}</h3>
+            </div>
+
+            <img className={'post_image'} src={imageUrl} alt={''}/>
+            
+            <h4 className={'post_text'}><strong>{username} </strong>{caption}</h4>
         </div>
     )
 }
